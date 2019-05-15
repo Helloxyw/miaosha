ee1cb9e6ff5e5f3053982e468e8a573aac863a31
RicardoXu <1641137052@qq.com>
2019-05-16 01:15:42 +0800

    Initial commit

---
+++ src/main/java/com/xyw/MainApplication.java
@@ -0,0 +1,16 @@
+package com.xyw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class MainApplication {
+
+    public static void main( String[] args ) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
